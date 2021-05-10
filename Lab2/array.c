a59cd23a9a7cfc9b75b4412799185b042ba65aeb
Hyun Ho <chh1880@gmail.com>
2021-05-10 17:01:29 +0900

    test

--- Lab2/array.c
+++ Lab2/array.c
@@ -3,8 +3,8 @@
 int get_index_of(const int numbers[], const size_t element_count, const int num)
 {
     size_t i = 0;
-    for(i = 0; i < element_count; ++i) {
-        if(numbers[i] == num) {
+    for (i = 0; i < element_count; ++i) {
+        if (numbers[i] == num) {
             return i;
         }
     }
@@ -14,17 +14,51 @@ int get_index_of(const int numbers[], const size_t element_count, const int num)
 
 int get_last_index_of(const int numbers[], const size_t element_count, const int num)
 {
-    return 0;
+    size_t i = 0;
+	int last_index = -1;
+    for (i = 0; i < element_count; ++i) {
+        if (numbers[i] == num) {
+            last_index = i;
+        }
+    }
+
+    return last_index;
 }
 
 int get_max_index(const int numbers[], const size_t element_count)
 {
-    return 0;
+    size_t i = 0;
+    int max_index = 0;
+	
+    if (element_count == 0) {
+        return -1;
+    }
+	
+    for (i = 0; i < element_count; ++i) {
+        if (max_index < numbers[i]) {
+            max_index = numbers[i];
+        }
+    }
+	
+    return max_index;
 }
 
 int get_min_index(const int numbers[], const size_t element_count)
 {
-    return 0;
+    size_t i = 0;
+    int min_index = 0;
+	
+    if (element_count == 0) {
+        return -1;
+    }
+	
+    while (i < element_count) {
+        if (min_index > numbers[i]) {
+            min_index = numbers[i];
+        }
+        ++i;
+    }
+    return min_index;
 }
 
 int is_all_positive(const int numbers[], const size_t element_count)
