9cbd74ff4c193653c566cad9375974f016c24f26
Hyun Ho <chh1880@gmail.com>
2021-05-26 21:04:16 +0900

    fdfsfsd

--- Lab4/main.c
+++ Lab4/main.c
@@ -1,79 +1,60 @@
-#include <stdio.h>
 #include <assert.h>
 #include <string.h>
-
 #include "my_string.h"
+
+#define _CRT_SECURE_NO_WARNINGS
 
 int main(void)
 {
-    char str[] = "X  my name is mui";
-    char single[] = "a";
-    char empty[] = "";
+    const char* str = "We all live in a yellow submarine";
+    char str_cpy[34];
+    const char* token;
 
-    char* token;
+    strncpy(str_cpy, str, 33);
+    str_cpy[33] = '\0';
 
-    /* REVERSE */
-    reverse(str);
-    assert(strcmp(str, "ium si eman ym  X") == 0);
-    reverse(str);
 
-    reverse_by_words(str);
-    assert(strcmp(str, "X  ym eman si ium") == 0);
-    reverse_by_words(str);
+    reverse(str_cpy);
+    assert(strcmp("enirambus wolley a ni evil lla eW", str_cpy) == 0);
 
-    reverse(single);
-    assert(strcmp(single, "a") == 0);
+    assert(index_of(str, "all ") == 3);
+    assert(index_of(str, "marine") == 27);
+    assert(index_of(str, "all good") == -1);
+    assert(index_of(str, "marinett") == -1);
 
-    reverse_by_words(single);
-    assert(strcmp(single, "a") == 0);
+    reverse(str_cpy);
 
-    reverse(empty);
-    assert(strcmp(empty, "") == 0);
+    assert(strcmp(str_cpy, str) == 0);
 
-    reverse_by_words(empty);
-    assert(strcmp(empty, "") == 0);
+    reverse_by_words(str_cpy);
 
-    /* INDEX OF */
-    /* assert(index_of(str, "my") == 3);
-    assert(index_of(str, "X") == 0);
-    assert(index_of(str, "") == 0);
-    assert(index_of(NULL, "X") == -1);
-    assert(index_of(str, "X    ") == -1);
-    assert(index_of(str, "mui") == 14);
-    assert(index_of(single, "a") == 0);
-    assert(index_of(empty, "") == 0);
-    assert(index_of("", "a") == -1);
-    assert(index_of("    ", "a") == -1); */
+    assert(strcmp("eW lla evil ni a wolley enirambus", str_cpy) == 0);
+    reverse_by_words(str_cpy);
 
-    /* TOKENIZER */ /*X  my name is mui*/
-    assert(tokenize(NULL, " !") == NULL);
+    assert(strcmp(str_cpy, str) == 0);
 
-    token = tokenize(str, " ");
-    assert(token == str);
-    assert(strcmp(token, "X") == 0);
+    assert(tokenize(NULL, " ") == NULL);
+
+    token = tokenize(str_cpy, " ");
+    assert(token == str_cpy);
+    assert(strcmp(token, "We") == 0);
 
     token = tokenize(NULL, " ");
-    assert(token == str + 3);
-    assert(strcmp(token, "my") == 0);
-
-    token = reverse_tokenize(NULL, " ");
-    assert(token == str + 6);
-    assert(strcmp(token, "eman") == 0);
+    assert(token == str_cpy + 3);
+    assert(strcmp(token, "all") == 0);
 
     token = tokenize(NULL, " ");
-    assert(token == str + 11);
-    assert(strcmp(token, "is") == 0);
-
-	printf("zzzzzzzzzzzzzz\n");
-    token = reverse_tokenize(NULL, " ");
-    assert(token == str + 14);
-    assert(strcmp(token, "ium") == 0);
+    assert(token == str_cpy + 7);
+    assert(strcmp(token, "live") == 0);
 
     token = reverse_tokenize(NULL, " ");
-    assert(token == NULL);
-    token = tokenize(NULL, " ");
-    assert(token == NULL);
+    assert(token == str_cpy + 12);
+    assert(strcmp(token, "ni") == 0);
 
-	printf("sadasd");
+    token = reverse_tokenize(NULL, " ");
+    token = reverse_tokenize(NULL, " ");
+    assert(token == str_cpy + 17);
+    assert(strcmp(token, "wolley") == 0);
+
     return 0;
 }
--- Lab4/my_string.c
+++ Lab4/my_string.c
@@ -114,30 +114,30 @@ char* tokenize(char* str_or_null, const char* delims)
 	
     /*Xa ,my name is mui*/
     while (*s_token_ptr != '\0') {		        
-	    delims_ptr = delims;
-	    while (*delims_ptr != '\0') {
-	        if (*s_token_ptr == *delims_ptr) {
-	            *s_token_ptr = '\0';				
-	            break;
-	        }
-	        ++delims_ptr;
-	    }		
-	    delims_ptr = delims;
-	    if (*s_token_ptr == '\0') {
-	        while (*delims_ptr != '\0') {
-	            if (*(s_token_ptr + 1) == *delims_ptr) {
-	                break;
-	            }
-	            ++delims_ptr;
-	        }			
-	        if (*delims_ptr == '\0') {
-	            ++s_token_ptr;
-	            printf("save_token : %s\n",save_token_ptr);
-	            printf("token_ptr : %c\n",*s_token_ptr);
-	            return (char*)save_token_ptr;
-	        }
-	    }		
-	    ++s_token_ptr;
+        delims_ptr = delims;
+        while (*delims_ptr != '\0') {
+            if (*s_token_ptr == *delims_ptr) {
+                *s_token_ptr = '\0';				
+                break;
+            }
+            ++delims_ptr;
+        }		
+        delims_ptr = delims;
+        if (*s_token_ptr == '\0') {
+            while (*delims_ptr != '\0') {
+                if (*(s_token_ptr + 1) == *delims_ptr) {
+                    break;
+                }
+                ++delims_ptr;
+            }			
+            if (*delims_ptr == '\0') {
+                ++s_token_ptr;
+                printf("save_token : %s\n",save_token_ptr);
+                printf("token_ptr : %c\n",*s_token_ptr);
+                return (char*)save_token_ptr;
+            }
+        }		
+        ++s_token_ptr;
     }
    
 		
