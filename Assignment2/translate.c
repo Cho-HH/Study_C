6cd0209fe75d7ade849dfe3ce2be92163360cfef
Hyun Ho <chh1880@gmail.com>
2021-06-03 16:45:44 +0900

    test

--- Assignment2/translate.c
+++ Assignment2/translate.c
@@ -60,6 +60,7 @@ int translate(int argc, const char** argv)
 {	
     size_t error = 0u;
     char str[MAX_BUFFER] = { 0, };
+	char str1[MAX_BUFFER] = { 0, };
     char argv1_buffer[MAX_BUFFER] = { 0, };
     char argv2_buffer[MAX_BUFFER] = { 0, };
     int argv1_len = 0;
@@ -72,7 +73,6 @@ int translate(int argc, const char** argv)
     
     if (argc == 3) {
         if (strlen(argv[1]) > MAX_BUFFER || strlen(argv[2]) > MAX_BUFFER) {
-            printf("ERROR_CODE_ARGUMENT_TOO_LONG");
             return ERROR_CODE_ARGUMENT_TOO_LONG;
         }
         
@@ -80,23 +80,17 @@ int translate(int argc, const char** argv)
         argv1_buffer[MAX_BUFFER - 1] = '\0';		
         error = escape_check(argv1_buffer);
         if (error == 1) {
-            printf("ERROR_CODE_INVALID_FORMAT");
             return ERROR_CODE_INVALID_FORMAT;
         }
-        
         argv1_len = strlen(argv1_buffer);
         
         strncpy(argv2_buffer, argv[2], argv1_len);
         argv2_buffer[argv1_len] = '\0';			
         error = escape_check(argv2_buffer);
         if (error == 1) {
-            printf("ERROR_CODE_INVALID_FORMAT");
             return ERROR_CODE_INVALID_FORMAT;
         }
         argv2_len = strlen(argv2_buffer);
-        
-        /*printf("first argv1_len : %d\n", argv1_len);
-        printf("first argv2_len : %d\n", argv2_len);*/
         
         if (argv1_len > argv2_len) {
             for (i = argv2_len; i < argv1_len; i++) {
@@ -113,26 +107,23 @@ int translate(int argc, const char** argv)
                 }
             }
         }
-        
-        /*printf("argv1 : %s, argv1_len : %d\n", argv1_buffer, strlen(argv1_buffer));
-        printf("argv2 : %s, argv2_len : %d\n", argv2_buffer, strlen(argv2_buffer));*/
-        while (1) {
+
+         while (1) {
             j = getchar();
             if (j == EOF) {
                 break;
             }
-            for (i = 0; *(argv1_buffer + i) != '\0'; i++) { /*argv[1][i]*/				
+            for (i = 0; *(argv1_buffer + i) != '\0'; i++) {		
                 if (j == *(argv1_buffer + i)) {
                     j = *(argv2_buffer + i);
                     break;
                 }
             }
             putchar(j);
-        }
+        } 
     } else if (argc == 4) {		
         if (argv[1][0] == '-' && argv[1][1] == 'i' && argv[1][2] == '\0') {
             if (strlen(argv[2]) > MAX_BUFFER || strlen(argv[3]) > MAX_BUFFER) {
-                printf("ERROR_CODE_ARGUMENT_TOO_LONG");
                 return ERROR_CODE_ARGUMENT_TOO_LONG;
             }
             
@@ -140,7 +131,6 @@ int translate(int argc, const char** argv)
             argv1_buffer[MAX_BUFFER - 1] = '\0';		
             error = escape_check(argv1_buffer);
             if (error == 1) {
-                printf("ERROR_CODE_INVALID_FORMAT");
                 return ERROR_CODE_INVALID_FORMAT;
             }
             
@@ -150,14 +140,10 @@ int translate(int argc, const char** argv)
             argv2_buffer[argv1_len] = '\0';			
             error = escape_check(argv2_buffer);
             if (error == 1) {
-                printf("ERROR_CODE_INVALID_FORMAT");
                 return ERROR_CODE_INVALID_FORMAT;
             }
             argv2_len = strlen(argv2_buffer);
-            
-            /*printf("first argv1_len : %d\n", argv1_len);
-            printf("first argv2_len : %d\n", argv2_len);*/
-            
+
             if (argv1_len > argv2_len) {
                 for (i = argv2_len; i < argv1_len; i++) {
                     argv2_buffer[i] = argv2_buffer[argv2_len - 1];
@@ -173,10 +159,7 @@ int translate(int argc, const char** argv)
                     }
                 }
             }
-            
-            /*printf("argv1 : %s, argv1_len : %d\n", argv1_buffer, strlen(argv1_buffer));
-            printf("argv2 : %s, argv2_len : %d\n", argv2_buffer, strlen(argv2_buffer));*/
-            
+
             while (1) {
                 j = getchar();
                 if (j == EOF) {
@@ -191,14 +174,11 @@ int translate(int argc, const char** argv)
                 putchar(j);
             }
         } else {
-            printf("ERROR_CODE_INVALID_FLAG");
             return ERROR_CODE_INVALID_FLAG;
         }
     } else {
-        printf("ERROR_CODE_WRONG_ARGUMENTS_NUMBER");
         return ERROR_CODE_WRONG_ARGUMENTS_NUMBER;
     }
-    
-    printf("%s\n", str);
+   
     return 0;
 }
