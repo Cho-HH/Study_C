e7389db50a953b227dac031ec782c286b9927ca8
Hyun Ho <chh1880@gmail.com>
2021-06-09 03:48:39 +0900

    test

--- Lab6/character_deserializer.c
+++ Lab6/character_deserializer.c
@@ -1,4 +1,4 @@
-/*#define _CRT_SECURE_NO_WARNINGS*/
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include "character_deserializer.h"
@@ -6,6 +6,7 @@
 void read_ver_1(character_v3_t* out_character, FILE* file);
 void read_ver_2(character_v3_t* out_character, FILE* file);
 void read_ver_3(character_v3_t* out_character, FILE* file);
+void check_name(char character_name[], const char* name);
 
 int get_character(const char* filename, character_v3_t* out_character)
 {
@@ -20,26 +21,15 @@ int get_character(const char* filename, character_v3_t* out_character)
 
     check_version = filename + strlen(filename) - 5;
 
-    switch (*check_version) {
-    case '1':
+    if (*check_version == '1') {
         read_ver_1(out_character, file);
-        fclose(file);
         return 1;
-        break;
-    case '2':
+    } else if (*check_version == '2') {
         read_ver_2(out_character, file);
-        fclose(file);
         return 2;
-        break;
-    case '3':
+    } else if (*check_version == '3') {
         read_ver_3(out_character, file);
-        fclose(file);
         return 3;
-        break;
-    default:
-        fclose(file);
-        return -1;
-        break;
     }
     
     return -1;
@@ -70,8 +60,8 @@ void read_ver_1(character_v3_t* out_character, FILE* file)
             out_character->level = value[i];
             out_character->leadership = value[i] / 10;
         } else if (strcmp(*(key + i), "id") == 0) {
-            sprintf(out_character->name, "%s%d", name, value[i]);	
-            out_character->name[MAX_NAME - 1] = '\0';			
+            sprintf(buffer, "%s%d", name, value[i]);
+            check_name(out_character->name, buffer);	
         } else if (strcmp(*(key + i), "str") == 0) {
             out_character->strength = value[i];
         } else if (strcmp(*(key + i), "dex") == 0) {
@@ -91,11 +81,17 @@ void read_ver_1(character_v3_t* out_character, FILE* file)
         } 
     } 
     out_character->minion_count = 0;	
+
+   
+    if (fclose(file) == EOF) {
+        perror("error while closing file");
+    }
 }
 
 void read_ver_2(character_v3_t* out_character, FILE* file)
 {
     char buffer[MAX_BUFFER] = { 0, };
+    char name_buffer[MAX_BUFFER] = { 0, };
     char* tmp = NULL;
     int magic_resist = 0;
     
@@ -109,18 +105,23 @@ void read_ver_2(character_v3_t* out_character, FILE* file)
         tmp++;
     }
 
-    sscanf(buffer, "%s %d %d %d %d %d %d %d %d %d", out_character->name, &out_character->level, &out_character->strength, &out_character->dexterity, &out_character->intelligence, &out_character->armour, &out_character->evasion, &magic_resist, &out_character->health, &out_character->mana);
-    out_character->name[MAX_NAME - 1] = '\0';
+    sscanf(buffer, "%s %d %d %d %d %d %d %d %d %d", name_buffer, &out_character->level, &out_character->strength, &out_character->dexterity, &out_character->intelligence, &out_character->armour, &out_character->evasion, &magic_resist, &out_character->health, &out_character->mana);
+    check_name(out_character->name, name_buffer);
     out_character->leadership = out_character->level / 10;
     out_character->minion_count = 0;
     out_character->elemental_resistance.fire = magic_resist / 3;
     out_character->elemental_resistance.cold = magic_resist / 3;
     out_character->elemental_resistance.lightning = magic_resist / 3;
+	
+    if (fclose(file) == EOF) {
+        perror("error while closing file");
+    }
 }
 
 void read_ver_3(character_v3_t* out_character, FILE* file)
 {
     char buffer[MAX_BUFFER] = { 0, };
+    char name_buffer[MAX_BUFFER] = { 0, };
     char* tmp = NULL;
     size_t i = 0;
     
@@ -135,8 +136,8 @@ void read_ver_3(character_v3_t* out_character, FILE* file)
         tmp++;
     }
     
-    sscanf(buffer, "%s %d %d %d %d %d %d %d %d %d %d %d %d %d", out_character->name, &out_character->level, &out_character->health, &out_character->mana, &out_character->strength, &out_character->dexterity, &out_character->intelligence, &out_character->armour, &out_character->evasion, &out_character->elemental_resistance.fire, &out_character->elemental_resistance.cold, &out_character->elemental_resistance.lightning, &out_character->leadership, &out_character->minion_count);
-    out_character->name[MAX_NAME - 1] = '\0';
+    sscanf(buffer, "%s %d %d %d %d %d %d %d %d %d %d %d %d %d", name_buffer, &out_character->level, &out_character->health, &out_character->mana, &out_character->strength, &out_character->dexterity, &out_character->intelligence, &out_character->armour, &out_character->evasion, &out_character->elemental_resistance.fire, &out_character->elemental_resistance.cold, &out_character->elemental_resistance.lightning, &out_character->leadership, &out_character->minion_count);
+    check_name(out_character->name, name_buffer);
 	
     if (out_character->minion_count > 0) {
         fgets(buffer, MAX_BUFFER, file);
@@ -149,8 +150,26 @@ void read_ver_3(character_v3_t* out_character, FILE* file)
                 }
                 tmp++;
             }
-            sscanf(buffer, "%s %d %d %d", out_character->minions[i].name, &out_character->minions[i].health, &out_character->minions[i].strength, &out_character->minions[i].defence);
-            out_character->minions[i].name[MAX_NAME - 1] = '\0';
+            sscanf(buffer, "%s %d %d %d", name_buffer, &out_character->minions[i].health, &out_character->minions[i].strength, &out_character->minions[i].defence);
+            check_name(out_character->minions[i].name, name_buffer);
         }
     }
+
+    if (fclose(file) == EOF) {
+        perror("error while closing file");
+    }
+}
+
+void check_name(char character_name[], const char* name)
+{  
+    const char* tmp = name;
+    while (*tmp != '\0') {
+        if ((*tmp < '0' || *tmp > '9') && (*tmp < 'A' || *tmp > 'Z') && *tmp != '_' && (*tmp < 'a' || *tmp > 'z')) {
+            return;
+        }
+        tmp++;
+    }
+    
+    sprintf(character_name, "%s", name);
+    character_name[MAX_NAME - 1] = '\0';
 }
