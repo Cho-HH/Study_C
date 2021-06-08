ca256326df0e6ce7413249330a15e8f9d3bb825f
Hyun Ho <chh1880@gmail.com>
2021-06-09 01:35:04 +0900

    test

---
+++ Lab6/character_deserializer.c
@@ -0,0 +1,165 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include "character_deserializer.h"
+
+void read_ver_1(character_v3_t* out_character, FILE* file);
+void read_ver_2(character_v3_t* out_character, FILE* file);
+void read_ver_3(character_v3_t* out_character, FILE* file);
+
+int get_character(const char* filename, character_v3_t* out_character)
+{
+    FILE* file;
+    const char* check_version = NULL;
+    
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("error while opening file");
+        return -1;
+    }
+    
+    check_version = filename + strlen(filename) - 5;
+    printf("version : %c\n", *check_version);
+    switch (*check_version) {
+    case '1':
+        read_ver_1(out_character, file);
+        return 1;
+        break;
+    case '2':
+        read_ver_2(out_character, file);
+        return 2;
+        break;
+    case '3':
+        read_ver_3(out_character, file);
+        return 3;
+        break;
+    default:
+        assert(0);
+        break;
+    }
+    
+    if (fclose(file) == EOF) {
+        perror("error while closing file");
+    }
+    
+    return 0;
+}
+
+void read_ver_1(character_v3_t* out_character, FILE* file)
+{
+    size_t i = 0;
+    char buffer[MAX_BUFFER] = { 0, };
+    char* tmp = NULL;
+    char key[8][MAX_BUFFER] = { 0, };
+    int value[8] = { 0, };
+    char name[MAX_NAME] = "player_";
+    
+    fgets(buffer, MAX_BUFFER, file);
+    tmp = buffer;
+    while (*tmp != '\0') {
+        if (*tmp == ':' || *tmp == ',') {
+            *tmp = ' ';			
+        }
+        tmp++;
+    }
+    
+    sscanf(buffer, "%s %d %s %d %s %d %s %d %s %d %s %d %s %d %s %d",
+        key[0], &value[0], key[1], &value[1], key[2], &value[2], key[3], &value[3], 
+        key[4], &value[4], key[5], &value[5], key[6], &value[6], key[7], &value[7]);
+    	
+    for (i = 0; i < 8; i++) {
+        if (strcmp(key[i], "lvl") == 0) {
+            out_character->level = value[i];
+            out_character->leadership = value[i] / 10;
+        } else if (strcmp(key[i], "id") == 0) {
+            sprintf(out_character->name, "%s%d", name, value[i]);			
+        } else if (strcmp(key[i], "str") == 0) {
+            out_character->strength = value[i];
+        } else if (strcmp(key[i], "dex") == 0) {
+            out_character->dexterity = value[i];
+            out_character->evasion = value[i] / 2;
+        } else if (strcmp(key[i], "intel") == 0) {
+            out_character->intelligence = value[i];
+        } else if (strcmp(key[i], "def") == 0) {
+            out_character->armour = value[i];
+            out_character->elemental_resistance.fire = value[i] / 12;
+            out_character->elemental_resistance.cold = value[i] / 12;
+            out_character->elemental_resistance.lightning = value[i] / 12;
+        } else if (strcmp(key[i], "hp") == 0) {
+            out_character->health = value[i];
+        } else if (strcmp(key[i], "mp") == 0) {
+            out_character->mana = value[i];
+        } 
+        printf("key : %s, value : %d\n", key[i], value[i]);
+    } 
+    out_character->minion_count = 0;	
+}
+
+void read_ver_2(character_v3_t* out_character, FILE* file)
+{
+    char buffer[MAX_BUFFER] = { 0, };
+    char* tmp = NULL;
+    int magic_resist = 0;
+    
+    fgets(buffer, MAX_BUFFER, file);
+    fgets(buffer, MAX_BUFFER, file);
+    tmp = buffer;
+    while (*tmp != '\0') {
+        if (*tmp == ',') {
+            *tmp = ' ';
+        }
+        tmp++;
+    }
+
+    sscanf(buffer, "%s %d %d %d %d %d %d %d %d %d",
+        out_character->name, &out_character->level, &out_character->strength,
+        &out_character->dexterity, &out_character->intelligence, &out_character->armour,
+        &out_character->evasion, &magic_resist, &out_character->health, &out_character->mana);
+    	
+    out_character->leadership = out_character->level / 10;
+    out_character->minion_count = 0;
+    out_character->elemental_resistance.fire = magic_resist / 3;
+    out_character->elemental_resistance.cold = magic_resist / 3;
+    out_character->elemental_resistance.lightning = magic_resist / 3;
+}
+
+void read_ver_3(character_v3_t* out_character, FILE* file)
+{
+    char buffer[MAX_BUFFER] = { 0, };
+    char* tmp = NULL;
+    size_t i = 0;
+    
+    fgets(buffer, MAX_BUFFER, file);
+    fgets(buffer, MAX_BUFFER, file);
+    
+    tmp = buffer;
+    while (*tmp != '\0') {
+        if (*tmp == '|') {
+            *tmp = ' ';
+        }
+        tmp++;
+    }
+    
+    sscanf(buffer, "%s %d %d %d %d %d %d %d %d %d %d %d %d %d",
+        out_character->name, &out_character->level, &out_character->health,
+        &out_character->mana, &out_character->strength, &out_character->dexterity,
+        &out_character->intelligence, &out_character->armour, &out_character->evasion,
+        &out_character->elemental_resistance.fire, &out_character->elemental_resistance.cold,
+        &out_character->elemental_resistance.lightning, &out_character->leadership, &out_character->minion_count);
+    
+    if (out_character->minion_count > 0) {
+        fgets(buffer, MAX_BUFFER, file);
+        for (i = 0; i < out_character->minion_count; i++) {
+            fgets(buffer, MAX_BUFFER, file);
+            tmp = buffer;
+            while (*tmp != '\0') {
+                if (*tmp == '|') {
+                    *tmp = ' ';
+                }
+                tmp++;
+            }
+            sscanf(buffer, "%s %d %d %d", out_character->minions[i].name, &out_character->minions[i].health, &out_character->minions[i].strength, &out_character->minions[i].defence);
+        }
+    }
+}
---
+++ Lab6/character_deserializer.h
@@ -0,0 +1,44 @@
+#ifndef CHARACTER_DESERIALIZER_H
+#define CHARACTER_DESERIALIZER_H
+
+enum { 
+    MAX_BUFFER = 512,
+    /*널문자 제외 50글자*/
+    MAX_NAME = 51,
+    MAX_MINION = 3
+};
+
+typedef struct {
+    unsigned int health;
+    unsigned int strength;
+    unsigned int defence;
+    char name[MAX_NAME];
+    char padding[2];
+} minion_t;
+
+typedef struct {
+    unsigned int fire;
+    unsigned int cold;
+    unsigned int lightning;
+} elemental_resistance_t;
+
+typedef struct {
+    char name[MAX_NAME];
+    char padding[2];
+    unsigned int level;
+    unsigned int health;
+    unsigned int mana;
+    unsigned int strength;
+    unsigned int dexterity;
+    unsigned int intelligence;
+    unsigned int armour;
+    unsigned int evasion;
+    unsigned int leadership;
+    size_t minion_count;
+    elemental_resistance_t elemental_resistance;
+    minion_t minions[MAX_MINION];
+} character_v3_t;
+
+int get_character(const char* filename, character_v3_t* out_character);
+
+#endif /* CHARACTER_DESERIALIZER_H */
