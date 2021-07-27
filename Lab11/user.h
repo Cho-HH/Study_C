06361d94b264d8a19fb8b55664207e08ca2430e1
Hyun Ho <chh1880@gmail.com>
2021-07-28 01:17:10 +0900

    no message

--- Lab11/user.h
+++ Lab11/user.h
@@ -2,13 +2,13 @@
 #define USER_H
 
 typedef struct {
-	size_t id;
-	char username[51];
-	char padding1;
-	char email[51];
-	char padding2;
-	char password[51];
-	char padding3;
-}user_t;
+    size_t id;
+    char username[51];
+    char padding1;
+    char email[51];
+    char padding2;
+    char password[51];
+    char padding3;
+} user_t;
 
 #endif /*USER_H*/
